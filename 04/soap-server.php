8cf5db56df466033d5c84b53ecd15b4d5ba5b33d
jeerawat.s <jeerawat.s@nidprotech.com>
2025-07-07 12:16:58 +0700

    add file

--- 04/soap-server.php
+++ 04/soap-server.php
@@ -1,13 +1,37 @@
 <?php
-class SoapService {
-    public function SayHello($request) {
+class SoapService
+{
+    public function SayHello($request)
+    {
         return ['greeting' => "Hello, {$request->name}!"];
     }
 
-      public function Add($request) {
+    public function Add($request)
+    {
         $a = $request->a;
         $b = $request->b;
         return ['result' => $a + $b];
+    }
+    public function Subtract($request)
+    {
+        $a = $request->a;
+        $b = $request->b;
+        return ['result' => $a - $b];
+    }
+    public function Multiply($request)
+    {
+        $a = $request->a;
+        $b = $request->b;
+        return ['result' => $a * $b];
+    }
+    public function Divide($request)
+    {
+        $a = $request->a;
+        $b = $request->b;
+        if ($b == 0) {
+            throw new SoapFault("Server", "Division by zero");
+        }
+        return ['result' => $a / $b];
     }
 }
 
